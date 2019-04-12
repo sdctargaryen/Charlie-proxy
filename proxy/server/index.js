d9522af3f99f1e4a2f1641e07ce3267ab53954af
charles-spinosa <charles.spinosa@gmail.com>
2019-04-12 13:29:20 -0700

    [ prog ] rough proxy

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.use('/api/gallery', proxy({ target: 'http://localhost:3001' }));
+app.use('/api/photos', proxy({ target: 'http://localhost:3001' }));
 app.use('/api/desc', proxy({ target: 'http://localhost:3002' }));
 app.use('/api/reservations', proxy({ target: 'http://localhost:3003' }));
-app.use('api/rating', proxy({ target: 'http://localhost:3004' }));
+app.use('/api/rating', proxy({ target: 'http://localhost:3004' }));
 app.use('/api/reviews', proxy({ target: 'http://localhost:3004' }));
 
 app.listen(port, () => {
