77a1612cc9ed33dd18ae90d905aa20de58559f32
charles-spinosa <charles.spinosa@gmail.com>
2019-04-12 12:13:58 -0700

    [ dev ] minor updates for dev configs

---
+++ proxy/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use('/api/gallery', proxy({ target: 'http://localhost:3001' }));
+app.use('/api/desc', proxy({ target: 'http://localhost:3002' }));
+app.use('/api/reservations', proxy({ target: 'http://localhost:3003' }));
+app.use('api/rating', proxy({ target: 'http://localhost:3004' }));
+app.use('/api/reviews', proxy({ target: 'http://localhost:3004' }));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
